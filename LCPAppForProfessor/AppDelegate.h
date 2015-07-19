301a64b963bd5dc9949b4f47897ba632267d87e7
陈天华 <chentianhua@chentianhuadeMacBook-Pro.local>
2015-07-19 12:28:35 +0800

    Initial Commit

---
+++ LCPAppForProfessor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCPAppForProfessor
+//
+//  Created by chentianhua on 15/7/19.
+//  Copyright © 2015年 chentianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCPAppForProfessor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCPAppForProfessor
+//
+//  Created by chentianhua on 15/7/19.
+//  Copyright © 2015年 chentianhua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
